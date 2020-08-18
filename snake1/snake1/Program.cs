e2216e7c8490f5c3305e8949eb313d4d50c86893
Oleona <Ole-onna@mail.ru>
2020-08-18 23:22:12 +0300

    class line

--- snake1/snake1/Program.cs
+++ snake1/snake1/Program.cs
@@ -10,91 +10,23 @@ namespace snake1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-            // int x1 = 1;
-            // int y1 = 3;
-            // char sym1 = '*';
-            // Console.SetCursorPosition(x1, y1);
-            // Console.Write(sym1);
-            // Drow(x1, y1, sym1);
-            //Point p1 = new Point();
+            
             Point p1 = new Point(1,3, '*');
-            /* p1.x = 1;
-             p1.y = 3;
-             p1.sym = '*';*/
-            //Drow(p1.x, p1.y, p1.sym);
             p1.Drow();
 
             
             Point p2 = new Point(4,5, '#');
             p2.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);
-
-            List<char> charList = new List<char>();
-            charList.Add('^');
-            List<char> charList1 = new List<char> { '&', '$', '*', '!', '@' };
-            charList.AddRange( charList1);
-            foreach (char i in charList)
-            {
-                Console.WriteLine(i);
-            }
-
-            Point p3 = new Point(7, 9, '!');
-            p3.Drow();
-
-            Point p4 = new Point(14, 7, '@');
-            p4.Drow();
-
-            Point p5 = new Point(9, 11, '$');
-            p5.Drow();
-
-            Point p6 = new Point(1, 14, '(');
-            p6.Drow();
-
-            plist.Add(p3);
-            plist.Add(p4);
-            plist.Add(p5);
-            plist.Add(p6);
-Console.WriteLine();
-            foreach (Point p in plist)
-            {
-                
-                Console.Write("{0} {1}: {2}", p.x, p.y, p.sym);
-                
-                Console.WriteLine();
-
-                string output = String.Format("x: {0}  y: {1} : sym :{2}", p.x, p.y, p.sym);
-                Console.WriteLine(output);
-
-
-            }
+            VerticalLine line1 = new VerticalLine(2, 7, 11, '*');
+            line1.Drow();
 
             Console.ReadLine();
         }
 
-        /*static void Drow(int x, int y, char sym)
-        {
- Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }*/
+        
     }
 }
---
+++ snake1/snake1/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake1
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x,int ydown, int yup,  char sym)
+        {
+            pList = new List<Point>();
+            for (int y = ydown; y <= yup; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+                p.Drow();
+        }
+    }
+}
+
