bf67b11645a352ae493c98b3ec5b1255c298fdbf
Oleona <Ole-onna@mail.ru>
2020-08-18 19:45:08 +0300

    list

--- snake1/snake1/Program.cs
+++ snake1/snake1/Program.cs
@@ -29,6 +29,65 @@ namespace snake1
             Point p2 = new Point(4,5, '#');
             p2.Drow();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
+
+            List<char> charList = new List<char>();
+            charList.Add('^');
+            List<char> charList1 = new List<char> { '&', '$', '*', '!', '@' };
+            charList.AddRange( charList1);
+            foreach (char i in charList)
+            {
+                Console.WriteLine(i);
+            }
+
+            Point p3 = new Point(7, 9, '!');
+            p3.Drow();
+
+            Point p4 = new Point(14, 7, '@');
+            p4.Drow();
+
+            Point p5 = new Point(9, 11, '$');
+            p5.Drow();
+
+            Point p6 = new Point(1, 14, '(');
+            p6.Drow();
+
+            plist.Add(p3);
+            plist.Add(p4);
+            plist.Add(p5);
+            plist.Add(p6);
+Console.WriteLine();
+            foreach (Point p in plist)
+            {
+                
+                Console.Write("{0} {1}: {2}", p.x, p.y, p.sym);
+                
+                Console.WriteLine();
+
+                string output = String.Format("x: {0}  y: {1} : sym :{2}", p.x, p.y, p.sym);
+                Console.WriteLine(output);
+
+
+            }
+
             Console.ReadLine();
         }
 
